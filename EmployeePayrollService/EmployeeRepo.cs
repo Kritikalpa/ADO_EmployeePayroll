107f81248107aaf7f5bcdca5d41176798fffd0b5
Kriti Kalpa <krtklpsh@gmail.com>
2020-11-03 21:57:52 +0530

    [Kriti] Add. DatabaseConnection

---
+++ EmployeePayrollService/EmployeeRepo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace EmployeePayrollService
+{
+    class EmployeeRepo
+    {
+        public static string connectionString = "Data Source=(LocalDb)\\testServer;Initial Catalog=payroll_services;Integrated Security=True";
+        SqlConnection connection = new SqlConnection(connectionString);
+    }
+}
