4be9ffd4a9d1c2acd1949e480e6ed3ee9a8c9954
Kriti Kalpa <krtklpsh@gmail.com>
2020-11-03 21:56:36 +0530

    first commit

---
+++ EmployeePayrollService/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace EmployeePayrollService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Payroll");
+           
+        }
+    }
+}
